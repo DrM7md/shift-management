105f16c1c17fdd526ec31dbfb1d661c71f04719b
MH <q6rrzz@gmail.com>
2026-02-14 11:44:16 +0300

    add build files

---
+++ public/build/assets/Warning-BabsieMG.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-Cwhk-y-Q.js";import{b as s}from"./Schedule-DiIcUQZb.js";const h=s(o.jsx("path",{d:"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8z"})),m=s(o.jsx("path",{d:"M1 21h22L12 2zm12-3h-2v-2h2zm0-4h-2v-4h2z"}));export{h as C,m as W};
